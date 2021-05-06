663165a851796e32197b1553e79d2addaa5b4bad
knownamenow <>
2021-05-07 02:58:11 +0700

    create task on tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.module';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private taskService: TasksService) { }
+
+	@Get()
+	getAllTasks(): Task[] {
+		return this.taskService.getAllTasks()
+	}
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.module';
+import { v1 as uuid } from 'uuid'
 @Injectable()
-export class TasksService {}
+export class TasksService {
+	private tasks: Task[] = []
+
+	getAllTasks(): Task[] {
+		return this.tasks
+	}
+
+	createTask(title: string, description: string): Task {
+		const task: Task = {
+			id: uuid(),
+			title,
+			description,
+			status: TaskStatus.DONE
+		}
+
+		this.tasks.push(task)
+
+		return task
+	}
+}
